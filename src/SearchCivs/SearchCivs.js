06ee41f7519a0b35f61523a89e8378c02ad3a7f4
Gabriel Freire <gabrbfreire@gmail.com>
2020-10-14 18:22:35 -0300

    Created civilizations lists

--- src/SearchCivs/SearchCivs.js
+++ src/SearchCivs/SearchCivs.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function SearchCivs() {
 
   const [query, setQuery] = useState('');
+  const [resultData, setData] = useState([]);
 
   const searchCivs = async (e) => {
     e.preventDefault();
@@ -18,17 +19,29 @@ function SearchCivs() {
     try {
       const response = await fetch(proxyUrl + url);
       const data = await response.json();
-      console.log(data);
+      setData(data.civilizations);
+      console.log(data.civilizations)
     } catch (err) {
       console.log(err);
     }
   }
 
+  const createList = () => {
+    let listTitle = '<table className="data-table"><tr><th>Name</th><th>Army Type</th><th>Team Bonus</th></tr>';
+    let listContent = resultData.map(item => "<tr><th>" + item.name + "</th><th>" + item.army_type + "</th><th>" + item.team_bonus + "</th></tr>");
+    return (listTitle + listContent + "</table>")
+  }
+
   return (
-    <form onSubmit={searchCivs} className="form-civs">
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}></input>
-      <button type="submit">Search</button>
-    </form>
+    <>
+      <form onSubmit={searchCivs} className="form-civs">
+        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}></input>
+        <button type="submit">Search</button>
+      </form>
+
+      <div dangerouslySetInnerHTML={{ __html: createList() }} />
+
+    </>
   )
 }
 
